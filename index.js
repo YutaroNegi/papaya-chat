6283168f45f858914529d8864fb2a9daf2855481
Yutaro Negi <yutaro.sousa@liveuniversity.com>
2022-05-29 11:01:12 -0300

    a

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '/front/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/front/build/index.html'));
+})
 
 app.get('/api/login', (req, res) => {
     res.send({userID: Math.round(Math.random() * 1000)})
